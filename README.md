d3064a2784d93a0020da65017187eccda7094087
Dan MacGuigan <daniel.macguigan@yale.edu>
2020-04-08 10:19:02 -0400

    second commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Visualizing 2019 Novel Coronavirus COVID-19 (2019-nCoV) daily data for the United States
+
+This repository contains scripts and data necessary to produce the following plots. All COVID-19 data is from the [Johns Hopkins CSSE github repository](https://github.com/CSSEGISandData/COVID-19). Map shapefiles are from (https://www.census.gov/)[https://www.census.gov/]. All scripts written for MacOSX 10.15.4.
+
+![Alt Text](https://github.com/dmacguigan/covid19Viz/tree/master/plots/US_covid-19_timelapse.gif)
+
+County data is only available starting on March 22.
+
+![Alt Text](https://github.com/dmacguigan/covid19Viz/tree/master/plots/US_covid-19_timelapse_counties.gif)
--- scripts/plotCovid19Cases.R
+++ scripts/plotCovid19Cases.R
@@ -403,10 +403,11 @@ for(i in 1:length(cleanCaseData)){
 
 setwd("/Users/dmacguigan/Documents/covid19/plots/dayPlots")
 
-system("convert -delay 40 *.png US_covid-19_timelapse.gif")
+# NEED TO UPDATE THE LAST FILE NAME AFTER "-delay 500" BASED ON CURRENT JHU REPO
+# second delay lets the gif pause on the final frame
+system("convert -delay 40 *.png -delay 500 04-07-2020.png US_covid-19_timelapse.gif")
 
 file.move("/Users/dmacguigan/Documents/covid19/plots/dayPlots/US_covid-19_timelapse.gif", "/Users/dmacguigan/Documents/covid19/plots/")
-
 
 ########################################################################################################################
 # plot Covid-19 case data by county
@@ -660,9 +661,11 @@ for(i in 1:length(cleanCaseData_county)){
 
 setwd("/Users/dmacguigan/Documents/covid19/plots/dayPlots_counties")
 
-system("convert -delay 40 *.png US_covid-19_timelapse_counties.gif")
+# NEED TO UPDATE THE LAST FILE NAME AFTER "-delay 500" BASED ON CURRENT JHU REPO
+# second delay lets the gif pause on the final frame
+system("convert -delay 40 *.png -delay 500 04-07-2020_counties.png US_covid-19_timelapse_counties.gif")
 
-file.move("/Users/dmacguigan/Documents/covid19/plots/dayPlots/US_covid-19_timelapse_counties.gif", "/Users/dmacguigan/Documents/covid19/plots/")
+file.move("/Users/dmacguigan/Documents/covid19/plots/dayPlots_counties/US_covid-19_timelapse_counties.gif", "/Users/dmacguigan/Documents/covid19/plots/")
 
 
 
